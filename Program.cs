9adf184849f9f227c5046e45aabd6a3103365048
TroyRatliff <142468266+TroyRatliff@users.noreply.github.com>
2024-02-19 15:04:27 -0500

    Update to sql query

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿SELECT Customers.CustomerName
+FROM Orders
+INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID
+WHERE Orders.OrderID = 10310;
+
+UNION
+
+SELECT Suppliers.Address
+FROM Suppliers
+WHERE Suppliers.SupplierID = (
+    SELECT Products.SupplierID
+    FROM Products
+    WHERE Products.ProductID = 40
+);
